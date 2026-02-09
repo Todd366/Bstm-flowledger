134e7924adec624fab7d80ff9865922d0634a1a5
Todd366 <myrahmosanako@gmail.com>
2026-02-09 18:02:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import './inject-fixes.js';   // <-- one line
 import './final-auth-inject.js';
+import './final-fix-inject.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
