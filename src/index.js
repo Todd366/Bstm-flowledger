eb37930c65703a459707c51c3310b00c7840ddfa
Todd366 <myrahmosanako@gmail.com>
2026-02-14 22:51:48 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import './simple-gate.js';  // active, crash-proof gate script
+import './app-fix-inject.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
