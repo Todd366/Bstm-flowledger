c612430110f17db87a6d9e1887705b6769225e7f
Todd366 <myrahmosanako@gmail.com>
2026-02-14 21:52:15 +0200

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import './inject-fixes.js';   // <-- one line
+import './final-auth-inject.js';
+import './access-gate.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
