cc515bb39321e04de7ea29b8d0099df02ad81944
BSTM Ecosystem <myrahmosanako@gmail.com>
2026-06-02 10:39:55 +0200

    Clean up index.js - remove gate scripts

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './simple-gate.js';  // active, crash-proof gate script
-import './app-fix-inject.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
